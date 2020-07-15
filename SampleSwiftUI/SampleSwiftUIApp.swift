2df131d81a401369afbf65234e0d3e1e980c37d6
fuwamaki <yusaku.maki@street-academy.com>
2020-07-16 00:00:57 +0900

    create Project

---
+++ SampleSwiftUI/SampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIApp.swift
+//  SampleSwiftUI
+//
+//  Created by yusaku maki on 2020/07/15.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
