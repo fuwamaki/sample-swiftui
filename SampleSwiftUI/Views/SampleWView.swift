047800e5a88a35f101a5d7815784790bb5699634
fuwamaki <yusaku.maki@street-academy.com>
2020-08-08 02:50:29 +0900

    realize drop and drag sample

--- SampleSwiftUI/Views/SampleWView.swift
+++ SampleSwiftUI/Views/SampleWView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SampleWView: View {
+    @State var images: [UIImage] = []
+
     var body: some View {
         HStack {
             VStack {
@@ -17,10 +19,13 @@ struct SampleWView: View {
                         .resizable()
                         .frame(width: 60, height: 60)
                     Text("Home")
+                    Text(String(images.count))
                 }
                 .frame(width: 100, height: 100)
                 .overlay(RoundedRectangle(cornerRadius: 8.0)
                             .stroke(Color.gray, lineWidth: 1))
+                .onDrop(of: ["public.image"],
+                        delegate: WMyDropDelegate(images: $images))
                 VStack {
                     Image(systemName: "car")
                         .resizable()
@@ -49,6 +54,41 @@ struct SampleWView: View {
                 Spacer()
             }
             .padding(.leading, 60)
+        }
+    }
+}
+
+struct WMyDropDelegate: DropDelegate {
+    @Binding var images: [UIImage]
+
+    func performDrop(info: DropInfo) -> Bool {
+        let gridPosition = getGridPosition(location: info.location)
+        print("WperformDrop")
+        if let item = info.itemProviders(for: ["public.image"]).first {
+            item.loadObject(ofClass: UIImage.self) { image, error in
+                if let image = image as? UIImage {
+                    self.images.append(image)
+                }
+            }
+            return true
+        } else {
+            return false
+        }
+    }
+
+    private func getGridPosition(location: CGPoint) -> Int {
+        let x = location.x - 464.0
+        let y = location.y - 65.0
+        if x > 150 && y > 150 {
+            return 4
+        } else if x > 150 && y < 150 {
+            return 3
+        } else if x < 150 && y > 150 {
+            return 2
+        } else if x < 150 && y < 150 {
+            return 1
+        } else {
+            return 0
         }
     }
 }
