de993c9cfb87becdb510ffc7e1c67ca38a26b296
fuwamaki <yusaku.maki@street-academy.com>
2020-08-08 15:50:37 +0900

    realize blur when dragging

--- SampleSwiftUI/Views/SampleWView.swift
+++ SampleSwiftUI/Views/SampleWView.swift
@@ -9,71 +9,103 @@ import SwiftUI
 
 struct SampleWView: View {
     @State var images: [UIImage] = []
+    @State var isDragging: Bool = false
 
     var body: some View {
-        HStack {
-            VStack {
-                Text("Driver")
+        ZStack {
+            if isDragging {
+                Color.gray
+                    .edgesIgnoringSafeArea(.all)
+            }
+            HStack {
                 VStack {
-                    ZStack {
-                        VStack {
-                            Image(systemName: "house")
-                                .resizable()
-                                .frame(width: 60, height: 60)
-                            Text("Home")
-                        }
-                        if (images.count > 0) {
-                            Text(String(images.count))
-                                .frame(width: 30, height: 30)
-                                .background(Color.red)
-                                .foregroundColor(.white)
-                                .font(.headline)
-                                .cornerRadius(15.0)
-                                .padding(.bottom, 62)
-                                .padding(.leading, 48)
+                    Text("Driver")
+                        .blur(radius: isDragging ? 10.0 : 0.0)
+                    VStack {
+                        ZStack {
+                            VStack {
+                                Image(systemName: "house")
+                                    .resizable()
+                                    .frame(width: 60, height: 60)
+                                Text("Home")
+                            }
+                            if (images.count > 0) {
+                                Text(String(images.count))
+                                    .frame(width: 30, height: 30)
+                                    .background(Color.red)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .cornerRadius(15.0)
+                                    .padding(.bottom, 62)
+                                    .padding(.leading, 48)
+                            }
                         }
                     }
+                    .frame(width: 100, height: 100)
+                    .background(Color.backgroundSecondary)
+                    .cornerRadius(13.0)
+                    .onDrop(of: ["public.image"],
+                            delegate: WMyDropDelegate(images: $images, isDragging: $isDragging))
+                    VStack {
+                        Image(systemName: "car")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("Car")
+                    }
+                    .frame(width: 100, height: 100)
+                    .background(Color.backgroundSecondary)
+                    .cornerRadius(13.0)
+                    Spacer()
                 }
-                .frame(width: 100, height: 100)
-                .overlay(RoundedRectangle(cornerRadius: 8.0)
-                            .stroke(Color.gray, lineWidth: 1))
-                .onDrop(of: ["public.image"],
-                        delegate: WMyDropDelegate(images: $images))
                 VStack {
-                    Image(systemName: "car")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                    Text("Car")
+                    Text("Home")
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Text("Taro")
+                            .font(.title)
+                    }
+                    .padding(8.0)
+                    .background(Color.white)
+                    .cornerRadius(8.0)
+                    .onDrag {
+                        let provider = NSItemProvider(object: UIImage(systemName: "home") ?? UIImage())
+                        provider.suggestedName = "Taro"
+                        print("startDrag")
+                        self.isDragging.toggle()
+                        return provider
+                    }
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Text("Hanako")
+                            .font(.title)
+                    }
+                    .padding(8.0)
+                    .background(Color.white)
+                    .cornerRadius(8.0)
+                    .onDrag {
+                        let provider = NSItemProvider(object: UIImage(systemName: "home") ?? UIImage())
+                        provider.suggestedName = "Hanako"
+                        print("startDrag")
+                        self.isDragging.toggle()
+                        return provider
+                    }
+                    Spacer()
                 }
-                .frame(width: 100, height: 100)
-                .overlay(RoundedRectangle(cornerRadius: 8.0)
-                            .stroke(Color.gray, lineWidth: 1))
-                Spacer()
+                .padding(.leading, 60)
+                .blur(radius: isDragging ? 10.0 : 0.0)
             }
-            VStack {
-                Text("Home")
-                HStack {
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                    Text("Taro")
-                        .font(.title)
-                }
-                .onDrag {
-                    let provider = NSItemProvider(object: UIImage(systemName: "home") ?? UIImage())
-                    provider.suggestedName = "Taro"
-                    print("startDrag")
-                    return provider
-                }
-                Spacer()
-            }
-            .padding(.leading, 60)
         }
+        
     }
 }
 
 struct WMyDropDelegate: DropDelegate {
     @Binding var images: [UIImage]
+    @Binding var isDragging: Bool
 
     func performDrop(info: DropInfo) -> Bool {
         if let item = info.itemProviders(for: ["public.image"]).first {
@@ -81,6 +113,7 @@ struct WMyDropDelegate: DropDelegate {
                 if let image = image as? UIImage {
                     self.images.append(image)
                     print(item.suggestedName)
+                    self.isDragging.toggle()
                 }
             }
             return true
