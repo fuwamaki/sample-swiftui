e80df505e7d36c68d1f39f9a8e3fdf97e829bfa9
fuwamaki <yusaku.maki@street-academy.com>
2020-08-08 15:50:32 +0900

    add Colors

---
+++ SampleSwiftUI/Extension/Color+Addition.swift
@@ -0,0 +1,26 @@
+//
+//  Color+Addition.swift
+//  SampleSwiftUI
+//
+//  Created by yusaku maki on 2020/08/08.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundPrimary = Color("BackgroundPrimary")
+    static let backgroundSecondary = Color("BackgroundSecondary")
+    static let backgroundTertiary = Color("BackgroundTertiary")
+    static let backgroundQuaternary = Color("BackgroundQuaternary")
+
+    static let textPrimary = Color("TextPrimary")
+    static let textSecondary = Color("TextSecondary")
+    static let textGray = Color("TextGray")
+    static let textReverse = Color("TextReverse")
+
+    static let leaf = Color("Leaf")
+    static let grape = Color("Grape")
+    static let lemon = Color("Lemon")
+    static let aqua = Color("Aqua")
+    static let salmon = Color("Salmon")
+}
