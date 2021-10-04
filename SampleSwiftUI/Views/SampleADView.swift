a7a9129dcdd978aa752a9fe6fcd0c5f4a7cd6ad9
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-10-04 14:56:50 +0900

    realize move object in SampleAD

--- SampleSwiftUI/Views/SampleADView.swift
+++ SampleSwiftUI/Views/SampleADView.swift
@@ -12,15 +12,31 @@ struct SampleADView: View {
     @State private var canvasView = CustomCanvasView()
     @ObservedObject private var viewModel = SampleADViewModel()
 
+    @State private var location: CGPoint = CGPoint(x: 0, y: 0)
+
     var body: some View {
         VStack {
-            CanvasView(canvasView: $canvasView, viewModel: viewModel)
-            Button(action: {
-                canvasView.drawing = PKDrawing()
-            }, label: {
-                Text("Clear")
-            })
+            Text("x: \(location.x)")
+            Text("y: \(location.y)")
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 64, height: 64)
+                .position(location)
+                .gesture(
+                    DragGesture().onChanged
+                    { value in
+                        location = value.location
+                    }
+                )
         }
+//        VStack {
+//            CanvasView(canvasView: $canvasView, viewModel: viewModel)
+//            Button(action: {
+//                canvasView.drawing = PKDrawing()
+//            }, label: {
+//                Text("Clear")
+//            })
+//        }
     }
 }
 
